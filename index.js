ad5734533e4631ce335f49ccfabf8e033cae8563
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-02-15 21:59:43 +0545

    fs module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-console.log(process.argv[3]);
-console.log("|hello world");
-
-
+const fs = require('fs');
+const path = require('path')
+const dirpath = path.join(__dirname,"files")
+for(i=0;i<5;i++){
+    fs.writeFileSync(dirpath+`/hello.txt${i}`,"a simple text file")
+}
 
 
