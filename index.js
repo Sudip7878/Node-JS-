3a9c4c3080395989c905872c6b714056891891b2
Sudip7878 <112951953+Sudip7878@users.noreply.github.com>
2024-02-15 22:34:56 +0545

    Add files via upload
    This is working with fs module

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const http = require('http')
-const data = require('./data')
+const path = require('path')
+const fs = require('fs')
+const dirpath = path.join(__dirname,"crud")
+const filepath = `${dirpath}/apple.txt`
 
-http.createServer((req,res)=>{
-    res.writeHead(200,"ok")
-    res.write(JSON.stringify(data))
-    res.end()
-}).listen(5000)
+fs.readFile(filepath,'utf8',(err,item)=>{
+console.log(item);
+})
+fs.writeFileSync(folderpath,"this is apple")
+
+fs.appendFile(filepath,"who are you",()=>{
+    if(!err) console.log('file is updated');
+})
+
+fs.rename(filepath,`${dirpath}/fruit.txt`,(err)=>{
+    if(!err) console.log('file has been renamed');
+    
+})
+
+fs.unlinkSync(`${dirpath}/fruit.txt`,(err)=>{
+    if(!err) console.log('file has been deleted');
+    
+})
