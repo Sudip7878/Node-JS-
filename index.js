475c6c4843aa640282304f1f271d853fa4b4dd9c
Sudip7878 <112951953+Sudip7878@users.noreply.github.com>
2024-04-12 23:47:42 +0545

    Create the server

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
-const path = require('path')
-const dirpath = path.join(__dirname,"files")
-for(i=0;i<5;i++){
-    fs.writeFileSync(dirpath+`/hello.txt${i}`,"a simple text file")
-}
+const http = require('http');
 
-
+http.createServer((req,res)=>{
+    res.write("");
+    res.end();
+}).listen(4502);
